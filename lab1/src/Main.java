0e316412f282d3be99b610cfaeb41c20a6354b28
Valentyn Serediuk <serediukit@gmail.com>
2023-11-20 18:55:23 +0200

    update file

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -2,8 +2,8 @@ import util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ExpTest test1 = new ExpTest("exp_results");
-        NormTest test2 = new NormTest("norm_results");
-        UniformTest test3 = new UniformTest("uniform_results");
+        new ExpTest("exp_results");
+        new NormTest("norm_results");
+        new UniformTest("uniform_results");
     }
 }
