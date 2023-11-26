1855717594a54c82e4a447615961d8bf286846b0
Valentyn Serediuk <serediukit@gmail.com>
2023-11-27 00:16:34 +0200

    task 1 completed

--- lab2/src/task1/Main.java
+++ lab2/src/task1/Main.java
@@ -1,6 +1,8 @@
+package task1;
+
 public class Main {
     public static void main(String[] args) {
-        Model model = new Model(2,1,5);
+        Model model = new Model(2, 1, 5);
         model.simulate(1000);
     }
 }
---
+++ lab2/src/task1/Model.java
@@ -0,0 +1,101 @@
+package task1;
+
+public class Model {
+    private double tnext;
+    private double tcurr;
+    private double t0;
+    private double t1;
+    private double delayCreate;
+    private double delayProcess;
+    private int numCreate;
+    private int numProcess;
+    private int failure;
+    private int state;
+    private int maxqueue;
+    private int queue;
+    private int nextEvent;
+
+    public Model(double delay0, double delay1) {
+        delayCreate = delay0;
+        delayProcess = delay1;
+        tnext = 0.0;
+        tcurr = tnext;
+        t0 = tcurr;
+        t1 = Double.MAX_VALUE;
+        maxqueue = 0;
+    }
+    public Model(double delay0, double delay1, int maxQ) {
+        delayCreate = delay0;
+        delayProcess = delay1;
+        tnext = 0.0;
+        tcurr = tnext;
+        t0 = tcurr;
+        t1 = Double.MAX_VALUE;
+        maxqueue = maxQ;
+    }
+
+    public void simulate(double timeModeling) {
+        while (tcurr < timeModeling) {
+
+            tnext = t0;
+            nextEvent = 0;
+
+            if (t1 < tnext) {
+                tnext = t1;
+                nextEvent = 1;
+            }
+            tcurr = tnext;
+            switch (nextEvent) {
+                case 0: event0();
+                    break;
+                case 1: event1();
+
+            }
+            printInfo();
+        }
+        printStatistic();
+    }
+
+    public void printStatistic() {
+        System.out.println("numCreate = " + numCreate
+                + " numProcess = " + numProcess
+                + " failure = " + failure
+        );
+    }
+    public void printInfo() {
+        System.out.println("t = " + tcurr
+                + " state = " + state
+                + " queue = " + queue
+        );
+    }
+    public void event0() {
+        t0 = tcurr + getDelayOfCreate();
+        numCreate++;
+        if (state == 0) {
+            state = 1;
+            t1 = tcurr + getDelayOfProcess();
+        } else {
+            if(queue < maxqueue)
+                queue++;
+            else
+                failure++;
+        }
+    }
+    public void event1() {
+        t1 = Double.MAX_VALUE;
+        state = 0;
+        if (queue > 0) {
+            queue--;
+            state = 1;
+            t1 = tcurr + getDelayOfProcess();
+        }
+        numProcess++;
+    }
+
+    private double getDelayOfCreate() {
+        return FunRand.Exp(delayCreate);
+    }
+    private double getDelayOfProcess() {
+        return FunRand.Exp(delayProcess);
+    }
+}
