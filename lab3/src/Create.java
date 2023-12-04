d78bb7896df4dd8ffe9e58d54498265cb62ec54c
Valentyn Serediuk <serediukit@gmail.com>
2023-12-04 17:49:10 +0200

    add files from lab2

---
+++ lab3/src/Create.java
@@ -0,0 +1,14 @@
+public class Create extends Element {
+    public Create(String name, double delay) {
+        super(name, delay);
+        super.setTnext(0.0); // імітація розпочнеться з події Create
+    }
+
+    @Override
+    public void outAct() {
+        super.outAct();
+        super.setTnext(super.getTcurr() + super.getDelay());
+        if (super.getNextElement() != null)
+            super.getNextElement().inAct();
+    }
+}
